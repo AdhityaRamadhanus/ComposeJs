6e971d5f02f5d31ea12112b4143588a3c56dd79d
Adhitya Ramadhanus <adhitya.ramadhanus@gmail.com>
2016-09-08 00:24:40 +0700

    Update compose function

--- compose.js
+++ compose.js
@@ -1,13 +1,12 @@
 'use strict'
-var slice = Function.prototype.call.bind(Array.prototype.slice);
-module.exports = function composeJs() {
-	var funs = slice(arguments).reverse()
+var slice = Function.prototype.call.bind(Array.prototype.slice)
+module.exports = (...args) => {
+	var funs = args.reverse()
 	if (funs.length === 0) throw new Error('No Functions Provided')
-	return function (){
-		var args = slice(arguments)
+	return (...innerargs) => {
 		return funs.slice(1).reduce(function (prev, curr){
 			if (curr.length > 1) throw new Error('Function need 2 or more arguments')
 			return curr(prev)
-		},funs[0].apply(funs[0], args))
+		},funs[0].apply(funs[0], innerargs))
 	}
 } 
